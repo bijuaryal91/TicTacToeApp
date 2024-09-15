1d719a22dff5af6843446d1a7c124c1fca6a078b
bijuaryal17@gmail.com <bijuaryal17@gmail.com>
2024-09-15 20:39:30 +0545

    Basic UI For Tic Tac Toe

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -18,7 +18,66 @@ class _TicAppState extends State<TicApp> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Text("Hi"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+              child: GridView.builder(
+                  padding: EdgeInsets.all(20),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemCount: 9,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () => {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue.shade50,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Colors.blue,
+                            width: 4,
+                          ),
+                        ),
+                        child: const Center(
+                          child: Text("X",
+                              style: TextStyle(
+                                  fontSize: 48, fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                    );
+                  })),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                const Text(
+                  "X Wins",
+                  style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 20),
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  child: const Text('Restart Game'),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
